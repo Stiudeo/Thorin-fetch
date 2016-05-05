b42eebe98d8760ce364b4968b3d7a45c53c2f803
snupa <bunta.adrian@gmail.com>
2016-05-05 14:32:11 +0300

    added file uploading with fetch.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,12 @@
 'use strict';
 import 'whatwg-fetch';
 const LOADED_FETCHERS = {};
-/**
- * The thorin fetcher create() function will create a named fetcher object and return it.
- * Each fetcher instance can be used separately with different configurations.
- *
- * CONFIGURATION ARGUMENTS:
- *  - url (string) - the full URL of thorin's /dispatch endpoint (defaults to window URL + '/dispatch
- *  - headers (object)  - additional headers to send
- *  - authorization (string) - an additional Authorization: Bearer {token} to attach
- *  - credentials (boolean) - should we send the cookies when calling a different url? defaults to false
-* */
-function createFetcher(config) {
+
+function parseConfig(config) {
   if(!config.url) config.url = window.location.href.split(window.location.pathname)[0] + '/dispatch';
+  let tmp = config.url.split('://'),
+    full = tmp[0] + '://'+ tmp[1].replace(/\/\//g,'/');
+  config.url = full;
   if(typeof config.headers !== 'object' || !config.headers) config.headers = {};
   config.headers['Accept'] = 'application/json';
   config.headers['Content-Type'] = 'application/json';
@@ -25,6 +19,41 @@ function createFetcher(config) {
   if(typeof config.credentials !== 'string') {
     config.credentials = 'same-origin';
   }
+  return config;
+}
+
+function parseError(e) {
+  let err;
+  if(typeof e === 'object' && e) {
+    if(e instanceof Error) {
+      err = e;
+    } else {
+      err = new Error(e.message || 'Failed to complete fetch request.');
+    }
+  } else {
+    e = {};
+    err = new Error(e.message || 'Failed to complete fetch request');
+  }
+  Object.keys(e).forEach((key) => {
+    err[key] = e[key];
+  });
+  if(!err.code) err.code = 'SERVER_ERROR';
+  if(!err.status) err.status = 500;
+  return err;
+}
+
+/**
+ * The thorin fetcher create() function will create a named fetcher object and return it.
+ * Each fetcher instance can be used separately with different configurations.
+ *
+ * CONFIGURATION ARGUMENTS:
+ *  - url (string) - the full URL of thorin's /dispatch endpoint (defaults to window URL + '/dispatch
+ *  - headers (object)  - additional headers to send
+ *  - authorization (string) - an additional Authorization: Bearer {token} to attach
+ *  - credentials (boolean) - should we send the cookies when calling a different url? defaults to false
+* */
+function createFetcher(config) {
+  parseConfig(config);
   /* This is the fetcher wrapper. */
   function doFetch(action, payload) {
     if(typeof action !== 'string') {
@@ -61,22 +90,7 @@ function createFetcher(config) {
         }
         resolve(res);
       }).catch((e) => {
-        let err;
-        if(typeof e === 'object' && e) {
-          if(e instanceof Error) {
-            err = e;
-          } else {
-            err = new Error(e.message || 'Failed to complete fetch request.');
-          }
-        } else {
-          e = {};
-          err = new Error(e.message || 'Failed to complete fetch request');
-        }
-        Object.keys(e).forEach((key) => {
-          err[key] = e[key];
-        });
-        if(!err.code) err.code = 'SERVER_ERROR';
-        if(!err.status) err.status = statusCode;
+        let err = parseError(e);
         reject(err);
       });
     });
@@ -99,13 +113,63 @@ function createFetcher(config) {
 }
 
 /**
+* Prepares a file upload fetcher.
+ * This works perfectly with thorin-plugin-upload
+ * SAME configuration
+* */
+function createUploadFetcher(config) {
+  parseConfig(config);
+  if(!config.name) config.name = 'asset'; // the name of the file input
+  delete config.headers['Content-Type'];
+  const obj = {};
+  /*
+  * Creates an actual fetch request to be sent.
+  * */
+  obj.send = function SendUpload(fileObj) {
+    return new Promise((resolve, reject) => {
+      if(typeof fileObj !== 'object' || !fileObj || typeof fileObj.type !== 'string' || typeof fileObj.name !== 'string') {
+        return reject(parseError(new Error('Please select a file to upload.')));
+      }
+      var data = new FormData()
+      data.append(config.name, fileObj);
+      let statusCode, statusMsg;
+      const fetchOpt = {
+        method: 'POST',
+        headers: config.headers,
+        credentials: config.credentials,
+        body: data
+      }
+      fetch(config.url, fetchOpt).then((res) => {
+        statusCode = res.status;
+        statusMsg = res.statusText;
+        return res.json();
+      }).then((res) => {
+        if(res.error) {
+          throw res.error;
+        }
+        delete res.type;
+        if(typeof res.meta === 'undefined') {
+          return resolve(res.result);
+        }
+        resolve(res);
+      }).catch((e) => {
+        let err = parseError(e);
+        reject(err);
+      });
+    });
+  }
+
+  return obj;
+}
+
+/**
 * This is the implicit fetcher creator.
  * Arguments:
  *  - name (string) if specified with no options, it will try returning the given fetcher by name or null.
  *  - name (object) if specified as an object, it will return a fetcher instance withouth caching it.
  *  - opt (object) - used with name, creates and saves a fetcher instance.
 * */
-module.exports = function(name, opt) {
+function create(name, opt) {
   // RETURN a fetcher.
   if(typeof name === 'string' && typeof opt === 'undefined') {
     return LOADED_FETCHERS[name] || null;
@@ -125,4 +189,9 @@ module.exports = function(name, opt) {
     return fetcherObj;
   }
   console.error('thorin-fetcher: invalid arguments for fetcher()');
-};
+}
+module.exports = create;
+/*
+* Attach the createUploadFetcher functionality
+* */
+module.exports.upload = createUploadFetcher;
